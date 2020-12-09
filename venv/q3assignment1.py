06f4d2be267c1e6a51d134c29edb91b3f44e9ac6
masterchief164 <rishugupta2001.rg@gmail.com>
2020-12-09 19:03:28 +0530

    q3 completed

---
+++ venv/q3assignment1.py
@@ -0,0 +1,10 @@
+# write your code here..
+q1,ms,q2,es,lw,pw=input().split()
+q1=float(q1)
+q2=float(q2)
+ms=float(ms)
+es=float(es)
+lw=float(lw)
+pw=float(pw)
+total=(q1/20)*.5+(ms/50)*2+(q2/20)*.5+(es/100)*3+(lw/100)*2+(pw/50)*2
+print("%.2f"%total)
