ef073ddb974598cf65ceb717b54e7550e6cd691d
masterchief164 <rishugupta2001.rg@gmail.com>
2020-12-09 16:36:29 +0530

    q1 completed

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# start your code here..
+print('"Never say \'No\',Never say \'I cannot\', \n \t for you are infinite.\n \n All the power is within you.You can \n \t do anything." \n \n \t \t - \\Swami Vivekanand\\')
