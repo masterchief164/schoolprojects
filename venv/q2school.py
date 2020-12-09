e9e3e8913e3ab73165f237c7b50241c868b7a662
masterchief164 <rishugupta2001.rg@gmail.com>
2020-12-09 17:31:24 +0530

    q2 completed

---
+++ venv/q2school.py
@@ -0,0 +1,9 @@
+PI = 3.14 # don't modify this value
+# start your code here..
+a,b=input().split()
+c=float(a)
+s=float(b)
+c=PI*c*c
+s=s*s
+print("Area of Circle:","%.2f"%c)
+print("Area of Square:","%.2f"%s)
