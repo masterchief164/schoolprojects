a05689b255a019cfeff69fe1cd0a0e1a87c2b947
masterchief164 <rishugupta2001.rg@gmail.com>
2021-01-01 13:15:01 +0530

    assignment 4 done

--- l4q2.py
+++ l4q2.py
@@ -8,7 +8,7 @@ for i in range(n):
             s = "-" + s
     s = s[::-1] + chr(97 + n - 1 - i) + s
     print(s)
-for i in reversed(range(n-1)):
+for i in reversed(range(n - 1)):
     s = ""
     for j in range(2 * n - 2):
         if j % 2 == 0 and (j / 2 + i >= n - 1):
