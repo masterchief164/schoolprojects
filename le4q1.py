742b635b4e1630ef2173a17ec4d2b4e0d08eae1b
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-24 14:21:50 +0530

    input from file done

--- le4q1.py
+++ le4q1.py
@@ -1,10 +1,12 @@
 f = open("lists.txt", 'r')
 c = 0
 lst = []
-#key_lst = input().split(",")
+key_lst = (list(map(float, input().split(','))))
 for i in f:
     c += 1
     lst.append(i.strip("\\n"))
 tmp = []
 for i in lst:
     tmp.append(list(map(float, i.split(','))))
+print(tmp)
+print(key_lst)
