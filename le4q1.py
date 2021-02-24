08c62f106c40b61d4879758e60b4ce3f0fab77fc
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-24 14:41:00 +0530

    distance done

--- le4q1.py
+++ le4q1.py
@@ -10,3 +10,11 @@ for i in lst:
     tmp.append(list(map(float, i.split(','))))
 print(tmp)
 print(key_lst)
+
+
+def distance(list1, list2):
+    d = 0
+    for i in range(len(list1)):
+        d = d + ((list1[i] - list2[i]) ** 2)
+    d = d ** 0.5
+    return d
