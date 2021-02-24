b402b6536b534634284c7f0306d6e3ee326b5911
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-24 14:38:29 +0530

    distance done

--- le4q1.py
+++ le4q1.py
@@ -1,12 +1,21 @@
 f = open("lists.txt", 'r')
-c = 0
+n = 0
 lst = []
 key_lst = (list(map(float, input().split(','))))
 for i in f:
-    c += 1
+    n += 1
     lst.append(i.strip("\\n"))
 tmp = []
 for i in lst:
     tmp.append(list(map(float, i.split(','))))
 print(tmp)
+print(len(tmp))
 print(key_lst)
+
+
+def distance(list1, list2):
+    d = 0
+    for i in range(len(list1)):
+        d = d + ((list1[i] - list2[i]) ** 2)
+    d = d ** 0.5
+    return d
