1b12d27ce745c76a1c12299a19026ea52b66ae35
theanmolsharma <anmolsharma0234@gmail.com>
2021-02-24 14:25:52 +0530

    mean_list done

--- le4q1.py
+++ le4q1.py
@@ -1,3 +1,19 @@
+def mean_list(list):
+    temp = []
+    n = len(list[0])
+    for i in range(0, len(list)):
+        temp.append(0)
+
+    for i in list:
+        t = 0
+        for j in i:
+            temp[t] = temp[t] + j
+            t += 1
+
+    for i in range(0, len(temp)):
+        temp[i] = temp[i] / n
+
+
 f = open("lists.txt", 'r')
 c = 0
 lst = []
@@ -10,3 +26,4 @@ for i in lst:
     tmp.append(list(map(float, i.split(','))))
 print(tmp)
 print(key_lst)
+
